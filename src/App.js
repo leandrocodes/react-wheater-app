823a5a80c5b01e16eedf88f4b169ed7e88cb1955
leandrocodes <leandrovianacodes@gmail.com>
2020-10-01 12:37:52 -0400

    :art: Weather conditions images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const api = {
+  key: '7b26e7b383af27d1169ebff362c62d15',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <main>
+        <div className="seach-box">
+          <input type="text" 
+            className="seach-bar" 
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
